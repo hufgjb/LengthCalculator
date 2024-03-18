27f6a1e9be52bb484a9300b9302aee8d83cb9eb5
huf <hufgjb226@gmail.com>
2024-03-18 14:34:40 +0800

    2024.03.18 improved some codes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,140 +30,193 @@ namespace LengthCalculator
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtCM = new System.Windows.Forms.TextBox();
+            this.txtM = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtKM = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtYard = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtFt = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.txtIn = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.btnAllClear = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(97, 106);
+            this.label1.Location = new System.Drawing.Point(44, 147);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(164, 24);
+            this.label1.Size = new System.Drawing.Size(206, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Centimeter (CM)";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // txtCM
             // 
-            this.textBox1.Location = new System.Drawing.Point(119, 149);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(97, 22);
-            this.textBox1.TabIndex = 1;
+            this.txtCM.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtCM.Location = new System.Drawing.Point(74, 201);
+            this.txtCM.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCM.Name = "txtCM";
+            this.txtCM.Size = new System.Drawing.Size(128, 34);
+            this.txtCM.TabIndex = 1;
             // 
-            // textBox2
+            // txtM
             // 
-            this.textBox2.Location = new System.Drawing.Point(295, 149);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(97, 22);
-            this.textBox2.TabIndex = 3;
+            this.txtM.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtM.Location = new System.Drawing.Point(308, 201);
+            this.txtM.Margin = new System.Windows.Forms.Padding(4);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(128, 34);
+            this.txtM.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(291, 106);
+            this.label2.Location = new System.Drawing.Point(303, 147);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 24);
+            this.label2.Size = new System.Drawing.Size(130, 31);
             this.label2.TabIndex = 2;
             this.label2.Text = "Meter (M)";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox3
+            // txtKM
             // 
-            this.textBox3.Location = new System.Drawing.Point(480, 149);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(97, 22);
-            this.textBox3.TabIndex = 5;
+            this.txtKM.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtKM.Location = new System.Drawing.Point(555, 201);
+            this.txtKM.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKM.Name = "txtKM";
+            this.txtKM.Size = new System.Drawing.Size(128, 34);
+            this.txtKM.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.Location = new System.Drawing.Point(476, 106);
+            this.label3.Location = new System.Drawing.Point(550, 147);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(149, 24);
+            this.label3.Size = new System.Drawing.Size(190, 31);
             this.label3.TabIndex = 4;
             this.label3.Text = "Kilometer (KM)";
             // 
-            // textBox4
+            // txtYard
             // 
-            this.textBox4.Location = new System.Drawing.Point(480, 292);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(97, 22);
-            this.textBox4.TabIndex = 11;
+            this.txtYard.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtYard.Location = new System.Drawing.Point(555, 370);
+            this.txtYard.Margin = new System.Windows.Forms.Padding(4);
+            this.txtYard.Name = "txtYard";
+            this.txtYard.Size = new System.Drawing.Size(128, 34);
+            this.txtYard.TabIndex = 11;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label4.Location = new System.Drawing.Point(476, 249);
+            this.label4.Location = new System.Drawing.Point(550, 326);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(113, 24);
+            this.label4.Size = new System.Drawing.Size(123, 31);
             this.label4.TabIndex = 10;
-            this.label4.Text = "Centimeter";
+            this.label4.Text = "Yard (YD)";
             // 
-            // textBox5
+            // txtFt
             // 
-            this.textBox5.Location = new System.Drawing.Point(295, 292);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(97, 22);
-            this.textBox5.TabIndex = 9;
+            this.txtFt.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtFt.Location = new System.Drawing.Point(305, 370);
+            this.txtFt.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFt.Name = "txtFt";
+            this.txtFt.Size = new System.Drawing.Size(128, 34);
+            this.txtFt.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label5.Location = new System.Drawing.Point(291, 249);
+            this.label5.Location = new System.Drawing.Point(303, 326);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(113, 24);
+            this.label5.Size = new System.Drawing.Size(115, 31);
             this.label5.TabIndex = 8;
-            this.label5.Text = "Centimeter";
+            this.label5.Text = "Foot (FT)";
             // 
-            // textBox6
+            // txtIn
             // 
-            this.textBox6.Location = new System.Drawing.Point(119, 292);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(97, 22);
-            this.textBox6.TabIndex = 7;
+            this.txtIn.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtIn.Location = new System.Drawing.Point(74, 370);
+            this.txtIn.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(128, 34);
+            this.txtIn.TabIndex = 7;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label6.Location = new System.Drawing.Point(115, 249);
+            this.label6.Location = new System.Drawing.Point(68, 326);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(113, 24);
+            this.label6.Size = new System.Drawing.Size(112, 31);
             this.label6.TabIndex = 6;
-            this.label6.Text = "Centimeter";
+            this.label6.Text = "Inch (IN)";
+            // 
+            // btnAllClear
+            // 
+            this.btnAllClear.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnAllClear.Location = new System.Drawing.Point(50, 46);
+            this.btnAllClear.Name = "btnAllClear";
+            this.btnAllClear.Size = new System.Drawing.Size(159, 54);
+            this.btnAllClear.TabIndex = 12;
+            this.btnAllClear.Text = "Clear";
+            this.btnAllClear.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(68, 426);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(141, 29);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Description";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(73, 458);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(666, 34);
+            this.textBox1.TabIndex = 14;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 412);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(883, 515);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnAllClear);
+            this.Controls.Add(this.txtYard);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtIn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtKM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtM);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCM);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -174,17 +227,20 @@ namespace LengthCalculator
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtCM;
+        private System.Windows.Forms.TextBox txtM;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtKM;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtYard;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtFt;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txtIn;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAllClear;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
